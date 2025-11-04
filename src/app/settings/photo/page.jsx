44c23f18600127ec89a30331812a5519938d2e47
Tiya <123706794+tiyawt@users.noreply.github.com>
2025-11-04 13:27:08 +0700

    Force dynamic rendering for PhotoSettingsPage

--- src/app/settings/photo/page.jsx
+++ src/app/settings/photo/page.jsx
@@ -1,6 +1,6 @@
 import PhotoClient from "./PhotoClient";
 import { fetchProfile } from "../fetchProfile";
-
+export const dynamic = 'force-dynamic'
 export default async function PhotoSettingsPage() {
   const profile = await fetchProfile();
   return <PhotoClient user={profile} />;
