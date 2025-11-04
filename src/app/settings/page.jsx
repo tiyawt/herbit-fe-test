3fc8cefabc8027e3e74fca78bf823ddaff47aaee
Tiya <123706794+tiyawt@users.noreply.github.com>
2025-11-04 13:26:38 +0700

    Force dynamic rendering for SettingsPage

--- src/app/settings/page.jsx
+++ src/app/settings/page.jsx
@@ -1,6 +1,7 @@
 import SettingsClient from "./SettingsClient";
 import { fetchProfile } from "./fetchProfile";
 
+export const dynamic = 'force-dynamic'
 export default async function SettingsPage() {
   const profile = await fetchProfile();
   return <SettingsClient profile={profile} />;
